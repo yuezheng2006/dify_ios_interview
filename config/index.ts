ad19754dcba6e78448331e50aadb8ac9c5fcd855
yangyuezheng <yangyuezheng@bytedance.com>
2023-08-18 16:51:26 +0800

    feat: add apikey

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,7 @@
 import type { AppInfo } from '@/types/app'
 export const APP_ID = '2ac6468b-bfcc-4382-ba8d-5d498732d8c2'
 export const API_KEY = 'app-YOPWxFtOxSkmAPbhhhFa9kF2'
+
 export const API_URL = ''
 export const APP_INFO: AppInfo = {
   title: 'Chat APP',
